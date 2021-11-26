5e070fa2565986dc852146f4f22a11c18594fb18
1upD <1upderek@gmail.com>
2021-11-26 17:58:09 -0500

    CLASS_ALIEN_PREDATOR neutral towards CLASS_BARNACLE

--- sp/src/game/shared/hl2/hl2_gamerules.cpp
+++ sp/src/game/shared/hl2/hl2_gamerules.cpp
@@ -1872,7 +1872,7 @@ ConVar  alyx_darkness_force( "alyx_darkness_force", "0", FCVAR_CHEAT | FCVAR_REP
 		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_ALIEN_PREDATOR, D_LI, 0 );
 		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_ALIEN_FAUNA, D_HT, 0 );
 		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_ANTLION, D_HT, 0 );
-		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_BARNACLE, D_FR, 0 );
+		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_BARNACLE, D_NU, 0 );
 		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_BULLSEYE, D_NU, 0 );
 		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_BULLSQUID, D_HT, 0 );
 		CBaseCombatCharacter::SetDefaultRelationship( CLASS_ALIEN_PREDATOR, CLASS_CITIZEN_PASSIVE, D_HT, 0 );
