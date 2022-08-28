c370c98e725b75fe25540c0afd7a5ecbf9eb9687
Blixibon <acj3017@gmail.com>
2022-08-28 18:08:41 -0500

    Added soldier gibbing (disabled by default)

--- sp/src/game/server/hl2/npc_combine.cpp
+++ sp/src/game/server/hl2/npc_combine.cpp
@@ -1114,6 +1114,40 @@ bool CNPC_Combine::CanOrderSurrender()
 
 	return m_iCanOrderSurrender == TRS_TRUE;
 }
+
+ConVar npc_combine_gib( "npc_combine_gib", "0" );
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : &info - 
+// Output : Returns true on success, false on failure.
+//-----------------------------------------------------------------------------
+bool CNPC_Combine::ShouldGib( const CTakeDamageInfo &info )
+{
+	if ( !npc_combine_gib.GetBool() )
+		return false;
+
+	// If the damage type is "always gib", we better gib!
+	if ( info.GetDamageType() & DMG_ALWAYSGIB )
+		return true;
+
+	if ( info.GetDamageType() & ( DMG_NEVERGIB | DMG_DISSOLVE ) )
+		return false;
+
+	if ( ( info.GetDamageType() & ( DMG_BLAST | DMG_ACID | DMG_POISON | DMG_CRUSH ) ) && ( GetAbsOrigin() - info.GetDamagePosition() ).LengthSqr() <= 32.0f )
+		return true;
+
+	return false;
+}
+
+//------------------------------------------------------------------------------
+// Purpose: Override to do rebel specific gibs
+// Output :
+//------------------------------------------------------------------------------
+bool CNPC_Combine::CorpseGib( const CTakeDamageInfo &info )
+{
+	return BaseClass::CorpseGib( info );
+}
 #endif
 
 //-----------------------------------------------------------------------------
