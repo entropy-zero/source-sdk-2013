496b19bca9b918ccbf01f7de7a687c9a3448737a
1upD <1upderek@gmail.com>
2023-11-25 13:45:21 -0500

    Refactoring

--- sp/src/game/server/ez2/ez2_save_metadata.cpp
+++ sp/src/game/server/ez2/ez2_save_metadata.cpp
@@ -45,6 +45,9 @@ public:
 			// Map Version
 			pCustomSaveMetadata->SetInt( "mapversion", gpGlobals->mapversion );
 
+			// Map name
+			pCustomSaveMetadata->SetString("map", STRING(gpGlobals->mapname));
+
 			// UNDONE: Host Name
 			//{
 			//	char szHostName[128];
@@ -83,16 +86,20 @@ public:
 protected:
 
 	// Recursive function to rotate metadata files
-	void RotateFile(char name[MAX_PATH], char const* pchSaveFile, int save_history_count, int counter=1)
+	void RotateFile(char pszFilenameToRotate[MAX_PATH], char const* pchOriginalSaveFilename, int iSaveHistoryCount, int iCounter=1)
 	{
+		char pszNewMetadataFilename[MAX_PATH];
+		char pszNewSaveFilename[MAX_PATH];
+
+
 		// Base case: File does not exist
-		if (!filesystem->FileExists(name, "MOD"))
+		if (!filesystem->FileExists(pszFilenameToRotate, "MOD"))
 		{
 			return;
 		}
 
-		// If this file is not an autosave, do not rotate
-		const char* pszFileName = V_GetFileName(pchSaveFile);
+		// If this file is not an autosave or quicksave, do not rotate
+		const char* pszFileName = V_GetFileName(pchOriginalSaveFilename);
 		if (!(V_stricmp(pszFileName, "autosave.sav") == 0 || V_stricmp(pszFileName, "quick.sav") == 0))
 		{
 			return;
@@ -100,46 +107,40 @@ protected:
 
 		// autosave.txt -> autosave01.txt
 		// quick.txt -> quick01.txt
-		char pszNewMetadataFilename[MAX_PATH];
-		Q_StripExtension(pchSaveFile, pszNewMetadataFilename, sizeof(pszNewMetadataFilename));
+		Q_StripExtension(pchOriginalSaveFilename, pszNewMetadataFilename, sizeof(pszNewMetadataFilename));
 		Q_strlower(pszNewMetadataFilename);
-		Q_FixSlashes(name);
+		Q_FixSlashes(pszFilenameToRotate);
 
 		// If the counter is below 10, append a leading 0
-		const char* pszMetadataFileSuffix = counter < 10 ? CFmtStr("0%d.txt", counter) : CFmtStr("%d.txt", counter);
+		const char* pszMetadataFileSuffix = iCounter < 10 ? CFmtStr("0%d", iCounter) : CFmtStr("%d", iCounter);
 
 		Q_strncat(pszNewMetadataFilename, pszMetadataFileSuffix, sizeof(pszNewMetadataFilename));
+		Q_strncpy(pszNewSaveFilename, pszNewMetadataFilename, sizeof(pszNewMetadataFilename));
 
-		char pszNewSaveFilename[MAX_PATH];
-		Q_StripExtension(pchSaveFile, pszNewSaveFilename, sizeof(pszNewSaveFilename));
-		Q_strlower(pszNewSaveFilename);
-		Q_FixSlashes(name);
-
-		// If the counter is below 10, append a leading 0
-		const char* pszSaveFileSuffix = counter < 10 ? CFmtStr("0%d.sav", counter) : CFmtStr("%d.sav", counter);
-
-		Q_strncat(pszNewSaveFilename, pszSaveFileSuffix, sizeof(pszNewSaveFilename));
+		// Add file extensions
+		Q_strncat(pszNewMetadataFilename, ".txt", sizeof(pszNewMetadataFilename));
+		Q_strncat(pszNewSaveFilename, ".sav", sizeof(pszNewSaveFilename));
 
 		// If the save associated with the next filename exists, rotate the next filename first
 		if (filesystem->FileExists(pszNewSaveFilename, "MOD"))
 		{
-			RotateFile(pszNewMetadataFilename, pchSaveFile, save_history_count, counter + 1);
+			RotateFile(pszNewMetadataFilename, pchOriginalSaveFilename, iSaveHistoryCount, iCounter + 1);
 		}
 		else if(filesystem->FileExists(pszNewMetadataFilename, "MOD"))
 		{
-			Msg("File %s does not exist. Removing metadata files %s and %s\n", pszNewSaveFilename, name, pszNewMetadataFilename);
-			filesystem->RemoveFile(name, "MOD");
+			Msg("File %s does not exist. Removing metadata files %s and %s\n", pszNewSaveFilename, pszFilenameToRotate, pszNewMetadataFilename);
+			filesystem->RemoveFile(pszFilenameToRotate, "MOD");
 			filesystem->RemoveFile(pszNewMetadataFilename, "MOD");
 			return;
 		}
 		// If we have passed the save history count, remove the files instead of rotating
-		else if (counter > save_history_count)
+		else if (iCounter > iSaveHistoryCount)
 		{
-			Msg("Tried to rotate to save metadata file %s but save history count is %d. Removing file %s\n", pszNewMetadataFilename, save_history_count, name);
-			filesystem->RemoveFile(name, "MOD");
+			Msg("Tried to rotate to save metadata file %s but save history count is %d. Removing file %s\n", pszNewMetadataFilename, iSaveHistoryCount, pszFilenameToRotate);
+			filesystem->RemoveFile(pszFilenameToRotate, "MOD");
 			return;
 		}
 
-		filesystem->RenameFile(name, pszNewMetadataFilename, "MOD");
+		filesystem->RenameFile(pszFilenameToRotate, pszNewMetadataFilename, "MOD");
 	}
 } g_CustomSaveMetadata;
