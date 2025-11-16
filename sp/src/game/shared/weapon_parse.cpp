be39b62694ddc4764574a229e7c675fe343f81df
Blixibon <acj3017@gmail.com>
2025-11-15 19:07:44 -0600

    [MAPBASE] Fix map-specific weapon scripts not updating HUD icons

--- sp/src/game/shared/weapon_parse.cpp
+++ sp/src/game/shared/weapon_parse.cpp
@@ -312,6 +312,10 @@ bool ReadWeaponDataFromFileForSlot( IFileSystem* filesystem, const char *szWeapo
 		return false;
 
 #ifdef MAPBASE
+	// Reload HUD elements if we had a custom script
+	if ( pFileInfo->bCustom )
+		pFileInfo->bLoadedHudElements = false;
+
 	pFileInfo->bCustom = false;
 #endif
 	pFileInfo->Parse( pKV, szWeaponName );
@@ -356,6 +360,7 @@ bool ReadCustomWeaponDataFromFileForSlot( IFileSystem* filesystem, const char *s
 		return false;
 
 	pFileInfo->bCustom = true;
+	pFileInfo->bLoadedHudElements = false;
 	pFileInfo->Parse( pKV, szWeaponName );
 
 	pKV->deleteThis();
