68f8d8649b304f8f7f6a8ccfd04bbdfd88a49893
Blixibon <acj3017@gmail.com>
2026-03-20 10:53:53 -0500

    Duck Sliding: Integrate animation on weapon_oicw

--- sp/src/game/server/ez2/weapon_oicw.cpp
+++ sp/src/game/server/ez2/weapon_oicw.cpp
@@ -176,6 +176,9 @@ acttable_t	CWeaponOICW::m_acttable[] =
 	{ ACT_HL2MP_JUMP,                    ACT_HL2MP_JUMP_AR1,                    false },
 	{ ACT_HL2MP_WALK,					ACT_HL2MP_WALK_AR1,					false },
 	{ ACT_HL2MP_GESTURE_RANGE_ATTACK2,	ACT_HL2MP_GESTURE_RANGE_ATTACK2_AR1,    false },
+#ifdef EZ2
+	{ ACT_HL2MP_SLIDE,					ACT_HL2MP_SLIDE_AR1,					false },
+#endif
 #endif
 #endif
 };
